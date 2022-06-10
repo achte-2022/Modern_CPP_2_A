32c45fb179f8f7078798b7ffe4c03b24089ccd92
tejas <tej>
2022-06-10 03:07:31 -0700

    Update

---
+++ src/main.cc
@@ -0,0 +1,55 @@
+//Header Files
+#include <iostream>
+#include <cstdlib>
+#include <random>
+
+//Constants
+const int kMinNumber = 0;
+const int kMaxNumber = 99;
+
+//Main Function
+int main(){
+    //Random Number Generator
+    std::random_device rd;
+    std::uniform_int_distribution<int> rd_distribution(kMinNumber, kMaxNumber);
+    int computer_guess = rd_distribution(rd);
+
+    //User Guess
+    int user_guess = kMinNumber;
+    std::cout << "Enter your guess: ";
+    std::cin >> user_guess;
+    if (std::cin.fail()){
+        std::cout << "Error encountered, exiting..." << std::endl;
+        std::cout << computer_guess;
+        return EXIT_FAILURE;
+    }
+
+    //Turning on the game
+    bool is_game_on = true;
+    while (is_game_on){
+        if ((user_guess < kMinNumber) || (user_guess > kMaxNumber)){
+            std::cerr << "[WARNING] : Number must be between 0 and 99"<<std::endl;
+            std::cout << "Enter your guess: ";
+            std::cin >> user_guess;
+            if (std::cin.fail()){
+                std::cout << "Error encountered, exiting..." << std::endl;
+                std::cout << computer_guess;
+                return EXIT_FAILURE;
+            }
+        }
+        else {
+            if (user_guess < computer_guess){
+                std::cout << "Your guess is smaller than the actual guess." <<std::endl;
+            }
+            else if (user_guess > computer_guess){
+                std::cout << "Your guess is larger than the actual guess." <<std::endl;
+            }
+            else {
+                std::cout << "You have guessed correctly." << std::endl;
+                is_game_on = false;
+            }
+        }
+    }
+    std::cout << computer_guess;
+    return EXIT_SUCCESS;
+}
